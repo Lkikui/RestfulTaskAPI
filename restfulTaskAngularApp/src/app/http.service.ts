e7009cfe2fde37b8af567a6d49266b977c830e2a
LisaRyland <lisa.ryland@gmail.com>
2018-02-16 17:42:30 -0800

    added http service with get requests

--- restfulTaskAngularApp/src/app/http.service.ts
+++ restfulTaskAngularApp/src/app/http.service.ts
@@ -7,11 +7,17 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
     this.getTasks();
+    this.getTaskById();
   }
   getTasks(){
     // our http response is an Observable, store it in a variable
     let tempObservable = this._http.get('/tasks');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
     tempObservable.subscribe(data => console.log("Got our tasks!", data));
- }
+  }
+
+  getTaskById(){
+    let tempObservable = this._http.get('/tasks/:id');
+    tempObservable.subscribe(data => console.log("Got our task by id!", data));
+  }
 }
