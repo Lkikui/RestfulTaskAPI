084bcf5b67b9db3c72de2b3deb4791232973b0b9
LisaRyland <lisa.ryland@gmail.com>
2018-02-17 20:18:34 -0800

    added database data display to html

--- restfulTaskAngularApp/src/app/app.component.ts
+++ restfulTaskAngularApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,37 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  snacks: [string];
+  loggedIn: boolean;
+  
+  num: number;
+  randNum: number;
+  str: string;
+  first_name: string;
+
   title = 'app';
+  tasks = [];
+
   constructor(private _httpService: HttpService){}
+  // ngOnInit will run when the component is initialized, after the constructor method.
+  ngOnInit(){
+    this.getTasksFromService();
+
+    this.num = 7;
+    this.randNum = Math.floor((Math.random() * 2) + 1);
+    this.str = 'Hello Angular Developer!';
+    this.first_name = 'Alpha';
+
+    this.snacks = ["vanilla latte with skim milk", "brushed suede", "cookie"];
+    this.loggedIn = true;
+  }
+
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got our tasks!", data);
+      this.tasks = data;
+    })
+  }
 }
--- restfulTaskAngularApp/src/app/http.service.ts
+++ restfulTaskAngularApp/src/app/http.service.ts
@@ -6,18 +6,21 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getTaskById();
+    // this.getTasks();
+    // this.getTaskById();
   }
   getTasks(){
     // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
+    // let tempObservable = this._http.get('/tasks');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks');
   }
 
   getTaskById(){
-    let tempObservable = this._http.get('/tasks/:id');
-    tempObservable.subscribe(data => console.log("Got our task by id!", data));
+  //   let tempObservable = this._http.get('/tasks/:id');
+  //   tempObservable.subscribe(data => console.log("Got our task by id!", data));
+    return this._http.get('/tasks/:id');
   }
+
 }
