0c08dd5f653fd92481dfe4d3c76902d53f3ee881
LisaRyland <lisa.ryland@gmail.com>
2018-02-18 16:53:07 -0800

    added forms to add and edit tasks, and delete functionality

--- restfulTaskAngularApp/src/app/app.component.ts
+++ restfulTaskAngularApp/src/app/app.component.ts
@@ -6,34 +6,66 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   tasks = [];
+  newTask: any;
+  task: any;
+  editedTask: any;
 
   constructor(private _httpService: HttpService){}
   // ngOnInit will run when the component is initialized, after the constructor method.
   ngOnInit(){
-    // this.getTasksFromService();
+    this.newTask = {title: "", description: ""};
+    this.editedTask = {title: `${this.task.title}`, description: `${this.task.description}`};
   }
 
   getTasksFromService(){
     let observable = this._httpService.getTasks();
     observable.subscribe(data => {
-      console.log("Got our tasks!", data);
+      // console.log("Got our tasks!", data);
       this.tasks = data;
     })
 
   }
 
-  // getTaskFromServiceById(){
-  //   let observable = this._httpService.getTaskById();
+  onButtonClick(event){
+    // console.log("button clicked");
+    this.getTasksFromService();
+    this.task = {title: `asdfasdf`, description: `asdfasdf`}
+  }
+
+  onSubmit(){
+    let observable = this._httpService.addTask(this.newTask);
+    observable.subscribe(data => {
+      console.log("Got data from post back", data);
+      this.newTask = {title: "", description: ""}
+    })
+  }
+
+  onDelete(_id){
+    console.log(_id);
+    let observable = this._httpService.deleteTask(_id);
+    observable.subscribe(data => {
+      console.log("deleting post", data);
+    })
+  }
+
+  // onEditButtonClick(_id){
+  //   // console.log(_id);
+  //   let observable = this._httpService.getTaskById(_id);
   //   observable.subscribe(data => {
   //     console.log("Got task by id!", data);
-  //     this.tasks = data;
+  //     this.task = data;
   //   })
+    
   // }
-
-  onButtonClick(event){
-    console.log("button clicked");
-    this.getTasksFromService();
+  
+  onEdit(task, _id){
+    this.editedTask = task;
+    let observable = this._httpService.editTask(this.editedTask, _id);
+    observable.subscribe(data => {
+      console.log("Edited data: ", data);
+    })
   }
 }
--- restfulTaskAngularApp/src/app/http.service.ts
+++ restfulTaskAngularApp/src/app/http.service.ts
@@ -17,10 +17,27 @@ export class HttpService {
     return this._http.get('/tasks');
   }
 
-  getTaskById(){
+  getTaskById(_id){
   //   let tempObservable = this._http.get('/tasks/:id');
   //   tempObservable.subscribe(data => console.log("Got our task by id!", data));
-    return this._http.get('/tasks/:id');
+    return this._http.get(`/tasks/${_id}`);
+  }
+
+  addTask(newTask){
+    return this._http.post('/tasks', newTask);
+  }
+
+  editTask(task, _id){
+    console.log(`edited task received by service:`);
+    console.log(task);
+    console.log(`id received by service:`);
+    console.log(_id);
+    return this._http.put(`/tasks/${_id}`, task);
+  }
+
+  deleteTask(_id){
+    console.log(`service url: /tasks/${_id}`);
+    return this._http.delete(`/tasks/${_id}`);
   }
 
 }
